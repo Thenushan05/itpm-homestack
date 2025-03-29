41e979180a35befae29149f58d1f4c78399a53d6
Thenushan Sritharan <thenushan@codelantic.com>
2025-03-29 06:50:16 +0530

    changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,14 @@ import { Chart } from "primereact/chart";
 import React, { useState } from "react";
 import "../dashboard/dashboard.sass";
 import { ChartData, ChartOptions } from "chart.js";
+import { ApexOptions } from "apexcharts";
+
+import dynamic from "next/dynamic";
+
+// Dynamically import the ReactApexChart component
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 const lineData: ChartData = {
   labels: ["January", "February", "March", "April", "May", "June", "July"],
@@ -93,13 +101,180 @@ const Dashboard = () => {
 
     setLineOptions(lineOptions);
   };
+  const series = [75.55];
+  const options: ApexOptions = {
+    colors: ["#465FFF"],
+    chart: {
+      fontFamily: "Outfit, sans-serif",
+      type: "radialBar",
+      height: 330,
+      sparkline: {
+        enabled: true,
+      },
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -85,
+        endAngle: 85,
+        hollow: {
+          size: "80%",
+        },
+        track: {
+          background: "#E4E7EC",
+          strokeWidth: "100%",
+          margin: 5, // margin is in pixels
+        },
+        dataLabels: {
+          name: {
+            show: false,
+          },
+          value: {
+            fontSize: "36px",
+            fontWeight: "600",
+            offsetY: -40,
+            color: "#1D2939",
+            formatter: function (val) {
+              return val + "%";
+            },
+          },
+        },
+      },
+    },
+    fill: {
+      type: "solid",
+      colors: ["#465FFF"],
+    },
+    stroke: {
+      lineCap: "round",
+    },
+    labels: ["Progress"],
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleDropdown() {
+    setIsOpen(!isOpen);
+  }
+
+  function closeDropdown() {
+    setIsOpen(false);
+  }
 
   return (
-    <div className="grid">
-      <div className="col-12 xl:col-6">
-        <div className="card">
-          <h5>Sales Overview</h5>
-          <Chart type="line" data={lineData} options={lineOptions} />
+    <div className="dashboard-container-home">
+      {" "}
+      <div className="grid">
+        <div className="col-12 xl:col-6">
+          <div className="card">
+            <h5>Sales Overview</h5>
+            <Chart type="line" data={lineData} options={lineOptions} />
+          </div>
+        </div>
+        <div className="rounded-2xl border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-white/[0.03]">
+          <div className="px-5 pt-5 bg-white shadow-default rounded-2xl pb-11 dark:bg-gray-900 sm:px-6 sm:pt-6">
+            <div className="flex justify-between">
+              <div>
+                <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
+                  Monthly Target
+                </h3>
+                <p className="mt-1 font-normal text-gray-500 text-theme-sm dark:text-gray-400">
+                  Target you’ve set for each month
+                </p>
+              </div>
+            </div>
+            <div className="relative">
+              <div className="max-h-[330px]">
+                <ReactApexChart
+                  options={options}
+                  series={series}
+                  type="radialBar"
+                  height={330}
+                />
+              </div>
+              <span className="absolute left-1/2 top-full -translate-x-1/2 -translate-y-[95%] rounded-full bg-success-50 px-3 py-1 text-xs font-medium text-success-600 dark:bg-success-500/15 dark:text-success-500">
+                +10%
+              </span>
+            </div>
+            <p className="mx-auto mt-10 w-full max-w-[380px] text-center text-sm text-gray-500 sm:text-base">
+              You earn $3287 today, it&apos;s higher than last month. Keep up
+              your good work!
+            </p>
+          </div>
+
+          <div className="flex items-center justify-center gap-5 px-6 py-3.5 sm:gap-8 sm:py-5">
+            <div className="target-revenue-box">
+              <p className="mb-1 text-center text-gray-500 text-theme-xs dark:text-gray-400 sm:text-sm">
+                Target
+              </p>
+              <p className="flex items-center justify-center gap-1 text-base font-semibold text-gray-800 dark:text-white/90 sm:text-lg">
+                $20K
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M7.26816 13.6632..."
+                    fill="#D92D20"
+                  />
+                </svg>
+              </p>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="target-revenue-box">
+              <p className="mb-1 text-center text-gray-500 text-theme-xs dark:text-gray-400 sm:text-sm">
+                Revenue
+              </p>
+              <p className="flex items-center justify-center gap-1 text-base font-semibold text-gray-800 dark:text-white/90 sm:text-lg">
+                $20K
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M7.60141 2.33683..."
+                    fill="#039855"
+                  />
+                </svg>
+              </p>
+            </div>
+
+            <div className="divider"></div>
+
+            <div className="target-revenue-box">
+              <p className="mb-1 text-center text-gray-500 text-theme-xs dark:text-gray-400 sm:text-sm">
+                Today
+              </p>
+              <p className="flex items-center justify-center gap-1 text-base font-semibold text-gray-800 dark:text-white/90 sm:text-lg">
+                $20K
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M7.60141 2.33683..."
+                    fill="#039855"
+                  />
+                </svg>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
